3c959e24a09513c329fb6ffffa437f6da60cc442
Tlescaud <lescaud91@gmail.com>
2024-12-08 15:08:58 -0500

    Initial commit

---
+++ apostrophe.py
@@ -0,0 +1,5 @@
+message ="One of Python's strengths is its diverse community."
+print (message)
+
+message = 'The correct way to use a apostrophe is inside quotes, project completed'
+print(message)
